7bdc0a5545419922a17cf74b1933efaaf3896f84
Hemanth Reddy Kareddy <hemanthreddy.k@infosys.com>
2024-09-23 15:34:18 +0530

    Initial Commit

---
+++ iTunesSearch_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTunesSearch_SwiftUI
+//
+//  Created by Hemanth Reddy Kareddy on 23/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iTunesSearch_SwiftUI/iTunesSearch_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSearch_SwiftUIApp.swift
+//  iTunesSearch_SwiftUI
+//
+//  Created by Hemanth Reddy Kareddy on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct iTunesSearch_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
